d07616f844af4e96095f705821dbf2831a7a7514
JamesMarkWilton <james.mark.wilton@gmail.com>
2016-03-29 16:53:28 -0500

    The basic paths have been laid out and the first integration test is built/green

--- test/integration/basic_site_navigation_test.rb
+++ test/integration/basic_site_navigation_test.rb
@@ -3,14 +3,14 @@ require 'test_helper'
 class BasicSiteNavigation < ActionDispatch::IntegrationTest
   def test_I_can_go_to_all_the_parts_of_my_site
     page.visit "/"
-    assert_includes /welcome/i, page.body
+    assert_match /welcome/i, page.body
     page.visit "/calendar"
-    assert_includes /calendar/i, page.body
+    assert_match /calendar/i, page.body
     page.visit "/booking"
-    assert_includes /booking/i, page.body
-    page.visit "/venue"
-    assert_includes /inforamtion/i, page.body
+    assert_match /booking/i, page.body
+    page.visit "/information"
+    assert_match /information/i, page.body
     page.visit "/archive"
-    assert_includes /history/i, page.body
+    assert_match /history/i, page.body
   end
 end
